ac00e47f685a8c4caefab063963545048c565f14
Pedro Molina <pmolina2602@gmail.com>
2022-01-23 16:32:20 -0600

    changed server side functions order

--- bot.py
+++ bot.py
@@ -96,19 +96,19 @@ def show_timezones(message):
 def echo_all(message):
 	bot.reply_to(message, message.text)
 
-bot.infinity_polling()
+# SERVER SIDE
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+   bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+   return "Updated", 200
 
-# SERVER SIDE 
 @server.route("/")
 def webhook():
    bot.remove_webhook()
    bot.set_webhook(url='https://limitless-lake-47905.herokuapp.com/' + TOKEN)
    return "Running", 200
 
-@server.route('/' + TOKEN, methods=['POST'])
-def getMessage():
-   bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-   return "Updated", 200
-
 if __name__ == "__main__":
 	server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+
+bot.infinity_polling()
